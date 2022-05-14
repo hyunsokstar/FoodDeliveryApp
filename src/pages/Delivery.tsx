b92f6c1391de90f3a269d792dfbdfbdb4c9d9083
ohyunsok <hyunsokstar@gmail.com>
2022-05-14 16:55:28 +0900

    1th commit

---
+++ src/pages/Delivery.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Delivery = (props: Props) => {
+  return (
+    <View>
+      <Text>Delivery</Text>
+    </View>
+  )
+}
+
+export default Delivery
+
+const styles = StyleSheet.create({})
---
+++ src/pages/Ing.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Ing = (props: Props) => {
+  return (
+    <View>
+      <Text>Ing</Text>
+    </View>
+  )
+}
+
+export default Ing
